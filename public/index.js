2d43573b61797a032f497205087b14e06a0e71e4
Meerahbel <mirabelugoji@gmail.com>
2023-11-16 23:07:36 -0800

    mobile menu navigation done

--- public/index.js
+++ public/index.js
@@ -2,6 +2,9 @@ const burger = document.querySelector("#burger");
 const burger2 = document.querySelector("#burger2");
 const menu = document.querySelector("#menu");
 
+const navButtons = document.querySelectorAll(".nav_buttons");
+
+// hamburger actions
 burger.addEventListener("click", () => {
   if (menu.classList.contains("hidden")) {
     menu.classList.remove("hidden");
@@ -14,6 +17,7 @@ burger.addEventListener("click", () => {
   }
 });
 
+// close icon actions
 burger2.addEventListener("click", () => {
   if (menu.classList.contains("hidden")) {
     menu.classList.remove("hidden");
@@ -24,4 +28,17 @@ burger2.addEventListener("click", () => {
     burger2.classList.add("hidden");
     burger.classList.remove("hidden");
   }
+});
+
+// nav buttons actions
+navButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (menu.classList.contains("hidden")) {
+      menu.classList.remove("hidden");
+      burger.classList.add("hidden");
+    } else {
+      menu.classList.add("hidden");
+      burger.classList.remove("hidden");
+    }
+  });
 });
